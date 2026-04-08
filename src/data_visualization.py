f83e348a3ef47b8b5f1f4f3d14d0c2519597aad4
govahernandez1991 <govahernandez1991@gmail.com>
2026-04-07 21:06:12 -0500

    Se agregaron los gráficos adicionales  y la generación de los reportes automáticos con ydata-profiling con DVC

--- src/data_visualization.py
+++ src/data_visualization.py
@@ -2,6 +2,7 @@ import pandas as pd
 import matplotlib.pyplot as plt
 import seaborn as sns
 from pathlib import Path
+from ydata_profiling import ProfileReport
 
 
 def visualize_data(
@@ -29,6 +30,10 @@ def visualize_data(
 
     # Crear directorio de salida
     output_dir.mkdir(parents=True, exist_ok=True)
+
+    # Crear directorio de salida para los reportes
+    output_dir_report = project_root / "docs"
+    output_dir_report.mkdir(parents=True, exist_ok=True)
 
     # Lectura de datos
     df_creditos = pd.read_csv(datos_creditos, sep=";")
@@ -68,6 +73,50 @@ def visualize_data(
     plt.savefig(output_dir / "correlation_heatmap_tarjetas.png")
     plt.close()
 
+    # Importe solicitado por falta de pago
+    plt.figure(figsize=(8,5))
+    sns.boxplot(data=df_creditos, x="falta_pago", y="importe_solicitado")
+    plt.title("Importe solicitado según falta de pago")
+    plt.xlabel("Falta de pago")
+    plt.ylabel("Importe solicitado")
+    plt.tight_layout()
+    plt.savefig(output_dir / "boxplot_importe_falta_pago.png")
+    plt.close()
+
+    # Límite de crédito por nivel de tarjeta
+    plt.figure(figsize=(8,5))
+    sns.boxplot(data=df_tarjetas, x="nivel_tarjeta", y="limite_credito_tc")
+    plt.title("Límite de crédito según nivel de tarjeta")
+    plt.xlabel("Nivel de tarjeta")
+    plt.ylabel("Límite de crédito")
+    plt.tight_layout()
+    plt.savefig(output_dir / "boxplot_limite_nivel_tarjeta.png")
+    plt.close()
+
+    #Generación de los reportes
+
+    print("Generando reportes automáticos...")
+
+    # Reporte créditos
+    profile_creditos = ProfileReport(
+        df_creditos,
+        title="Reporte Perfilado - Créditos",
+        explorative=True
+    )
+
+    profile_creditos.to_file(output_dir_report / "reporte_creditos.html")
+
+
+    # Reporte tarjetas
+    profile_tarjetas = ProfileReport(
+        df_tarjetas,
+        title="Reporte Perfilado - Tarjetas",
+        explorative=True
+    )
+
+    profile_tarjetas.to_file(output_dir_report / "reporte_tarjetas.html")
+
+    print(f"Reportes guardados en: {output_dir_report}")
     print(f"Figuras guardadas en: {output_dir}")
 
 
